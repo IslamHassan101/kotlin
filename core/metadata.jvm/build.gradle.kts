ff9fbf5c6fce51f14963ca015e9540a48be590f5
Jinseong Jeon <jsjeon@google.com>
2020-02-02 23:34:10 -0800

    Avoid uses of plain "kotlin" in metadata deserialization.
    \#KT-35587 Fixed

--- core/metadata.jvm/build.gradle.kts
+++ core/metadata.jvm/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm")
     id("jps-compatible")
@@ -8,14 +10,20 @@ javaHome = rootProject.extra["JDK_16"] as String
 
 dependencies {
     compile(project(":core:metadata"))
+    testCompile(commonDep("junit:junit"))
 }
 
 sourceSets {
     "main" { projectDefault() }
-    "test" {}
+    "test" { projectDefault() }
 }
 
 tasks.withType<JavaCompile> {
     sourceCompatibility = "1.6"
     targetCompatibility = "1.6"
+}
+
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
